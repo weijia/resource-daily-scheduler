cbe914bd9fcc4cea9724f31e9319725120e2a425
Richard <richardwangwang@gmail.com>
2015-07-23 23:15:36 +0800

    Use new reversion feature to register to admin.

--- resource_daily_scheduler/admin.py
+++ resource_daily_scheduler/admin.py
@@ -1,13 +1,15 @@
 from djangoautoconf.auto_conf_admin_tools.admin_register import AdminRegister
+from djangoautoconf.auto_conf_admin_tools.reversion_feature import ReversionFeature
 
 import models
 factory = AdminRegister()
-
+factory.add_feature(ReversionFeature())
 factory.register_all_model(models)
-from reversion.helpers import patch_admin
 
-patch_admin(models.BookingRequest)
-patch_admin(models.BookableResource)
+# from reversion.helpers import patch_admin
+#
+# patch_admin(models.BookingRequest)
+# patch_admin(models.BookableResource)
 
 
 from reversion import models
